d13c259698785ddce842599a936df9423e7b1bbd
ShenghaoisYummy <hsupisces@hotmail.com>
2024-03-30 13:49:55 +1100

    NavBar init

--- taxpal/src/components/Header.jsx
+++ taxpal/src/components/Header.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import Container from "./Container";
+import Logo from "@/components/Logo";
+import Button from "@/components/Button";
+import Link from "next/link";
+import NavLink from "@/components/Navlink";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="py-10">
+      <Container>
+        <nav className="flex items-center md:gap-x-12">
+          <div>
+            <Link href="/" aria-label="Home">
+              <Logo className="h-10 w-auto"></Logo>
+            </Link>
+            <div className="hidden md:flex md:gap-x-6">
+              <NavLink href="#features">Features</NavLink>
+              <NavLink href="#testimonials">Testimonials</NavLink>
+              <NavLink href="#pricing">Pricing</NavLink>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-5 md:gap-x-8">
+            <div className="hidden md:block">
+              <NavLink href="/login">Sign In</NavLink>
+            </div>
+            <Button href="/register">
+              Get Started
+              <span className="hidden lg:inline">Today</span>
+            </Button>
+          </div>
+        </nav>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ taxpal/src/components/Navlink.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+const NavLink = ({ href, children }) => {
+  return (
+    <Link
+      href={href}
+      className="inline-block rounded-lg px-2 text-sm text-slate-700 hover:text-white"
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default NavLink;
