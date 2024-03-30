e632ff4c756dd44bcc9d712693ea164b3045b656
ShenghaoisYummy <hsupisces@hotmail.com>
2024-03-30 14:46:47 +1100

    popover completed

--- taxpal/src/components/Header.jsx
+++ taxpal/src/components/Header.jsx
@@ -4,16 +4,18 @@ import Logo from "@/components/Logo";
 import Button from "@/components/Button";
 import Link from "next/link";
 import NavLink from "@/components/Navlink";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
     <div className="py-10">
       <Container>
-        <nav className="flex items-center md:gap-x-12">
-          <div>
+        <nav className="flex justify-between relative z-50">
+          <div className="flex items-center md:gap-x-12">
             <Link href="/" aria-label="Home">
               <Logo className="h-10 w-auto"></Logo>
             </Link>
+
             <div className="hidden md:flex md:gap-x-6">
               <NavLink href="#features">Features</NavLink>
               <NavLink href="#testimonials">Testimonials</NavLink>
@@ -24,10 +26,15 @@ const Header = () => {
             <div className="hidden md:block">
               <NavLink href="/login">Sign In</NavLink>
             </div>
-            <Button href="/register">
-              Get Started
-              <span className="hidden lg:inline">Today</span>
+            <Button href="/register" color="blue">
+              <span>
+                Get Started
+                <span className="hidden lg:inline"> Today</span>
+              </span>
             </Button>
+            <div className="-mr-1 md:hidden">
+              <MobileNav />
+            </div>
           </div>
         </nav>
       </Container>
