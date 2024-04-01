7322b9649fbbe32ac91db7d000c7e0f3653ce161
ShenghaoisYummy <hsupisces@hotmail.com>
2024-04-01 11:52:44 +1100

    panel completed

--- taxpal/src/pages/index.jsx
+++ taxpal/src/pages/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Head from "@/components/Head";
 import Hero from "@/components/Hero";
+import PrimaryFeatures from "@/components/PrimaryFeatures";
 const Home = () => {
   return (
     <>
       <Head />
       <Hero />
+      <PrimaryFeatures />
     </>
   );
 };
