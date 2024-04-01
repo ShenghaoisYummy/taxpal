dd3254628134bd9ccc5783fe36f34fa6769c4aec
ShenghaoisYummy <hsupisces@hotmail.com>
2024-04-01 15:58:20 +1100

    Secondaryfeatures completed

--- taxpal/src/pages/index.jsx
+++ taxpal/src/pages/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Head from "@/components/Head";
 import Hero from "@/components/Hero";
 import PrimaryFeatures from "@/components/PrimaryFeatures";
+import SecondaryFeatures from "@/components/SecondaryFeatures";
 const Home = () => {
   return (
     <>
       <Head />
       <Hero />
       <PrimaryFeatures />
+      <SecondaryFeatures />
     </>
   );
 };
