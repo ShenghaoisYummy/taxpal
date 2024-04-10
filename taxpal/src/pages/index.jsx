651524bd97a84cd753c7975e86b6890ffb004953
ShenghaoisYummy <hsupisces@hotmail.com>
2024-04-10 15:02:25 +1000

    testimonials completed

--- taxpal/src/pages/index.jsx
+++ taxpal/src/pages/index.jsx
@@ -3,6 +3,10 @@ import Head from "@/components/Head";
 import Hero from "@/components/Hero";
 import PrimaryFeatures from "@/components/PrimaryFeatures";
 import SecondaryFeatures from "@/components/SecondaryFeatures";
+import Testimonials from "@/components/Testimonials";
+import Pricing from "@/components/Pricing";
+import Faqs from "@/components/Faqs";
+
 const Home = () => {
   return (
     <>
@@ -10,6 +14,9 @@ const Home = () => {
       <Hero />
       <PrimaryFeatures />
       <SecondaryFeatures />
+      <Testimonials />
+      <Pricing />
+      <Faqs />
     </>
   );
 };
